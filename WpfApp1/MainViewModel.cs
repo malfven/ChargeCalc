d6678ca5d41bd8e39a6466486b0152f632c32985
SEMIAL <mikael.alfven@hoganas.com>
2018-01-19 16:10:31 +0100

    Add project files.

---
+++ WpfApp1/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    
+    public class MainViewModel
+    {
+        private string mTest;
+
+        public MainViewModel()
+        {
+            Test = "kaka";
+        }
+
+        public string Test { get => mTest; set => mTest = value; }
+
+       
+    }
+}
